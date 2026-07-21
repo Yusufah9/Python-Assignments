36faf0a0b0a65de32f71ad7c725f2160f2a667ca
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-07-21 12:02:01 +0100

    Add fatherson and scores Python programs

--- AfterParty/fatherson.py
+++ AfterParty/fatherson.py
@@ -1,9 +1,10 @@
-father_age = int(input("Enter father's age"))
-son_age = int(input("Enter son\'\s age "))
+father_age = int(input("Enter father's age: "))
+son_age = int(input("Enter son's age: "))
 
-if father_age > son_age and son_age <= father_age:
-    int father_age / son_age or father_age - son_age
+if father_age > son_age:
+
+    age_ratio = int(father_age / son_age) 
 
 father_age_years_ago = father_age - son_age
-print(father_age_years_ago)
-    s 
+print(f"The father was that age {father_age_years_ago} years ago.")
+
--- AfterParty/scores.py
+++ AfterParty/scores.py
@@ -1,30 +1,21 @@
-
 grade1 = "A"
 grade2 = "B"
 grade3 = "C"
-grade4 = "Average"
+grade4 = "D"  # Changed from "Average" to match prompt requirements
 grade5 = "E"
-score = int(input("Enter your score between 0 - 100"))
-if (90 <= score and  score <= 100)
-print(grade1)
 
-elif(80 <= score and score <= 90)
-print(grade2)
+score = int(input("Enter your score between 0 - 100: "))
 
-elif(70 <= score and score <= 80)
-print(grade3)
+if 90 <= score <= 100:
+    print(grade1)
+elif 80 <= score < 90:   # Changed <= to < to avoid overlapping grades
+    print(grade2)
+elif 70 <= score < 80:
+    print(grade3)
+elif 60 <= score < 70:
+    print(grade4)
+elif 55 <= score < 60:
+    print(grade5)
+else:
+    print("Failing Grade")
 
-elif(60 <= score and score <= 70)
-print(grade4)
-
-else print("Average")
-
-
-#write a program that calculate the average of the three passed to it and returns the letter value associated with that
-#grade.
-#Numerical score                   Letter Grade
-#90 <= score <= 100                   A
-#80 <= score <= 90                    B
-#70 <= score <= 80                    C
-#60 <= score <= 70                    D
-#55 <= score <= 60                    E
