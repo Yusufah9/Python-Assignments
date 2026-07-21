a63f4c80fc565b3060ec7eebd602a5f0ab6acbf1
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-07-21 12:57:40 +0100

    Add class exercises in the  Class Assignment folders

---
+++ ClassAssignment/exercise4.py
@@ -0,0 +1,11 @@
+
+left = 27.5
+right = 2
+
+print(f"Addition (27.5 + 2): {left + right}")
+print(f"Subtraction (27.5 - 2): {left - right}")
+print(f"Multiplication (27.5 * 2): {left * right}")
+print(f"Division (27.5 / 2): {left / right}")
+print(f"Floor Division (27.5 // 2): {left // right}")
+print(f"Exponentiation (27.5 ** 2): {left ** right}")
+
---
+++ ClassAssignment/exercise5.py
@@ -0,0 +1,14 @@
+
+radius = 2
+pi = 3.14159
+
+
+diameter = 2 * radius
+circumference = 2 * pi * radius
+area = pi * (radius ** 2)
+
+# Display results
+print(f"Diameter: {diameter}")
+print(f"Circumference: {circumference}")
+print(f"Area: {area}")
+
