174299e26c31bff11d0d297f0c37f3923587c7e9
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-07-21 11:30:26 +0100

    Add fatherson and scores Python programs

---
+++ AfterParty/fatherson.py
@@ -0,0 +1,9 @@
+father_age = int(input("Enter father's age"))
+son_age = int(input("Enter son\'\s age "))
+
+if father_age > son_age and son_age <= father_age:
+    int father_age / son_age or father_age - son_age
+
+father_age_years_ago = father_age - son_age
+print(father_age_years_ago)
+    s 
---
+++ AfterParty/scores.py
@@ -0,0 +1,30 @@
+
+grade1 = "A"
+grade2 = "B"
+grade3 = "C"
+grade4 = "Average"
+grade5 = "E"
+score = int(input("Enter your score between 0 - 100"))
+if (90 <= score and  score <= 100)
+print(grade1)
+
+elif(80 <= score and score <= 90)
+print(grade2)
+
+elif(70 <= score and score <= 80)
+print(grade3)
+
+elif(60 <= score and score <= 70)
+print(grade4)
+
+else print("Average")
+
+
+#write a program that calculate the average of the three passed to it and returns the letter value associated with that
+#grade.
+#Numerical score                   Letter Grade
+#90 <= score <= 100                   A
+#80 <= score <= 90                    B
+#70 <= score <= 80                    C
+#60 <= score <= 70                    D
+#55 <= score <= 60                    E
