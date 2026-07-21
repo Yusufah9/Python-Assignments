fa0be602cc94f45b585cad1136aee70e8427a8c9
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-07-21 13:04:04 +0100

    Add class exercises in the  Class Assignment folders

---
+++ ClassAssignment/exercise6.py
@@ -0,0 +1,10 @@
+# Prompt user for an integer
+# Check if the remainder is 0
+
+number = int(input("Enter an integer:"))
+
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+
